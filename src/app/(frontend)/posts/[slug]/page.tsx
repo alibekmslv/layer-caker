f47d6ee6c95579b8e153eedbe6b7b39fa34e415c
Alibek Massalimov <alibekmslv@gmail.com>
2025-11-25 11:22:39 +0700

    Add Tailwind Typography support and enhance post rendering with Portable Text components

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { sanityFetch } from "@/sanity/lib/live";
 import { POST_QUERY } from "@/sanity/lib/queries";
 import { urlFor } from "@/sanity/lib/image";
+import { components } from "@/sanity/portableTextComponents";
+import { PortableText } from "next-sanity";
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
@@ -23,7 +25,7 @@ export default async function Page({
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
       {post?.mainImage ? (
         <Image
-          className="w-full aspect-[800/300]"
+          className="aspect-[800/300] mx-auto"
           src={urlFor(post.mainImage)
             .width(800)
             .height(300)
@@ -35,7 +37,12 @@ export default async function Page({
           height={300}
         />
       ) : null}
-      <h1 className="text-4xl font-bold text-balance">{post?.title}</h1>
+      <h1 className="text-4xl font-bold text-balance mx-auto">{post?.title}</h1>
+      {post?.body ? (
+        <div className="prose mx-auto">
+          <PortableText value={post.body} components={components} />
+        </div>
+      ) : null}
       <hr />
       <Link href="/posts">&larr; Return to index</Link>
     </main>
